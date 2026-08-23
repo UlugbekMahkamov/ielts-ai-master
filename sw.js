e0d794b6d79b7ee09fa59f8c7e269b588ca83b55
UlugbekMahkamov <115599233+UlugbekMahkamov@users.noreply.github.com>
2026-08-23 18:39:48 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE_NAME = 'ielts-master-v1';
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
